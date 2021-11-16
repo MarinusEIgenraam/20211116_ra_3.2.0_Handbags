e48b867e408842b7ca31b12074796db05644e058
MarinusEIgenraam <rinuseigenraam@gmail.com>
2021-11-16 18:15:00 +0100

    finished tiles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
-import Button from "./Components/Button/Button";
 
+///////////////////////
+//// Images
 import Handy from './assets/bag_1.png'
-import Stylish from './assets/bag_2.png'
-import Simple from './assets/bag_3.png'
-import Trendy from './assets/bag_4.png'
+import Stylish from './assets/bag_2.png';
+import Simple from './assets/bag_3.png';
+import Trendy from './assets/bag_4.png';
+import Brand from './assets/brand.png';
+import About from './assets/our_story.png';
+
+///////////////////////
+//// Components
 import Product from "./Components/Product/Product";
+import Button from "./Components/Button/Button";
+import Tile from "./Components/Tile/Tile";
 
 const navItems = [{
     title: 'to the collection',
@@ -25,27 +33,58 @@ const producten = [{
     special: 'Best seller',
     price: 400,
     img: Handy
-},{
+}, {
     title: 'The stylish bag',
     special: 'Best seller',
     price: 250,
     img: Stylish
 
-},{
+}, {
     title: 'The simple bag',
     special: 'New collection',
     price: 300,
     img: Simple
-},{
+}, {
     title: 'The trendy bag',
     special: 'New collection',
     price: 150,
     img: Trendy
 },]
 
+const tile = [{
+    image: '',
+    title: 'The Brand',
+    text: ['Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aspernatur culpa debitis\n' +
+    '                        delectus, dolor dolorum earum esse eveniet expedita illum in ipsum, libero maxime minus omnis\n' +
+    '                        quam recusandae repellendus sequi suscipit temporibus, velit veritatis vitae! Doloremque eos eum\n' +
+    '                        nihil rem.','Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aspernatur culpa debitis\n' +
+    '                        delectus, dolor dolorum earum esse eveniet expedita illum in ipsum, libero maxime minus omnis\n' +
+    '                        quam recusandae repellendus sequi suscipit temporibus, velit veritatis vitae! Doloremque eos eum\n' +
+    '                        nihil rem.'],
+
+}, {
+    image: Brand,
+    alt: 'Brand',
+    title: '',
+    text: [{}],
+}, {
+    image: About,
+    alt: 'About',
+    title: '',
+    text: [{}],
+}, {
+    image: '',
+    title: 'Our story',
+    text: ["Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aspernatur culpa debitis\n" +
+    "                        delectus, dolor dolorum earum esse eveniet expedita illum in ipsum, libero maxime minus omnis\n" +
+    "                        quam recusandae repellendus sequi suscipit temporibus, velit veritatis vitae! Doloremque eos eum\n" +
+    "                        nihil rem."],
+}]
+
 function App() {
     return (
         <fragment>
+            <h1>Handbags & Purses</h1>
             <nav className={"navbar"}>
                 {navItems.map(item => {
                     const {title, className, disabled} = item;
@@ -55,19 +94,7 @@ function App() {
                 })}
             </nav>
             <main>
-                <article>
-                    <span>
-                    </span>
-                    <img src={Trendy}/>
-                    <p>
-                        {producten[0].title}
-                    </p>
-                    <h1>
-                        {producten[0].price}
 
-                    </h1>
-
-                </article>
                 {producten.map(item => {
                     const {title, special, img, price} = item;
                     return (
@@ -75,7 +102,24 @@ function App() {
                     )
                 })}
             </main>
-            <h1>Handbags & Purses</h1>
+            <footer>
+                {tile.map((item) => {
+                    return (
+                        <section>
+                            {item.image ? <img src={item.image}/> :
+                                <div><h2>{item.title}</h2>{item.text.map((paragraph) => {
+                                    return (<p>{paragraph}</p>)
+                                })}</div>}
+                        </section>
+                    )
+                })}
+                {tile.map((item) => {
+                    const {image, title, text} = item;
+                    return (
+                        <Tile title={title} image={image} text={text}/>
+                    )
+                })}
+            </footer>
         </fragment>
 
     );
